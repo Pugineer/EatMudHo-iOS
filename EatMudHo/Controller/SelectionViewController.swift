b7f3e0eb31b71cd14885f3145d4d1868c87d3395
Chris <chrislau828@gmail.com>
2020-08-23 22:24:47 +0800

    Recommend View UI Auto Layout completed

---
+++ EatMudHo/Controller/SelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SelectionViewController.swift
+//  EatMudHo
+//
+//  Created by Chris on 23/8/2020.
+//  Copyright © 2020 Pixel Unity. All rights reserved.
+//
+
+import UIKit
+
+class SelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
